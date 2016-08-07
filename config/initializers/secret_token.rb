7927c063bc7cbcbd15d6aa0d24edcdf71bbac2b5
henry <siccha_17@hotmail.com>
2016-08-07 11:16:20 -0500

    creado estatus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deybook::Application.config.secret_token = '4711ef9649e7d43356297aa31df8499d93420070be89d1d93740b25a0d769b1dcd0a1efbe50e27805926b16f1f651b0896b6449bc5495b04ff048f53d415e2ed'
